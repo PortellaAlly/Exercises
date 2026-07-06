1f33b003b113339cefbe1ab5c3a3d832b13b9f6b
Portella <Allysonpalmeirim12@gmail.com>
2026-07-06 18:43:54 -0300

    feat(Listas) | added question 01 answer

---
+++ alura/desafioListasAula01/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Produto> produtos = new ArrayList<>();
+
+        Produto produto1 = new Produto("Sabao", 6.77, 13);
+        Produto produto2 = new Produto("Espada laser", 12.99, 22);
+
+        produtos.add(produto1); produtos.add(produto2);
+    }
+}
