ef28666a771e820458753d3e0f599ed536f602fc
Portella <Allysonpalmeirim12@gmail.com>
2026-07-09 17:56:42 -0300

    Feat(desafioFinalListas) | created an initializing to user interface

--- alura/desafioFinalListas/src/Main.java
+++ alura/desafioFinalListas/src/Main.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         Conta conta = new Conta();
+
+        System.out.print("Initializing");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.print(".");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.print(".");
+        TimeUnit.MILLISECONDS.sleep(1);
+        System.out.println(".");
+        TimeUnit.MILLISECONDS.sleep(1);
 
         while(conta.getLimite() <= 0) {
             System.out.println("Digite seu limite");
             conta.setLimite(scanner.nextInt());
         }
+
+
     }
 }
