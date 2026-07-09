e745e3dba279c4b89893cacf0504e2b55ec48885
Portella <Allysonpalmeirim12@gmail.com>
2026-07-09 18:23:48 -0300

    Feat(desafioFinalListas) | Added an basic logic to the buying system

--- alura/desafioFinalListas/src/Main.java
+++ alura/desafioFinalListas/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -21,6 +23,26 @@ public class Main {
         while(account.getLimit() <= 0) {
             System.out.println("Hello, " + account.getName() + ". Could you write down your credit card limit? (Don't use broken numbers)");
             account.setLimit(scanner.nextInt());
+        }
+
+        System.out.println("Do you want to start buying?(Y/N)");
+
+        String userDecicions = scanner.nextLine();
+        List<Products> products = new ArrayList<>();
+        int i = 0;
+
+        if(userDecicions == "Y"){
+            while (userDecicions == "Y") {
+                i++;
+
+                System.out.println("What is the product name? ");
+                
+                System.out.println("How much does it cost?");
+
+                System.out.println("Do you want to continue buying?(Y/N)");
+            }
+        } else if (userDecicions == "N") {
+            System.out.println(" s");
         }
 
 
