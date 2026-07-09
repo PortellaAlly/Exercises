b4a8b391a650e8553e196ef65339bc07cc2514f9
Portella <Allysonpalmeirim12@gmail.com>
2026-07-09 17:23:18 -0300

    feat(ListasAula04) | completed question 2. Fixed titulo constructor

--- alura/desafioListasAula04/src/Main.java
+++ alura/desafioListasAula04/src/Main.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // Question 1
         List<Integer> lista1 = new ArrayList<>();
 
         lista1.add(6);
@@ -15,5 +16,14 @@ public class Main {
         Collections.sort(lista1);
         System.out.println(lista1);
 
+        // Question 2
+        ArrayList<Titulo> titulos = new ArrayList<>();
+
+        titulos.add(new Titulo("Manifesto comunista"));
+        titulos.add(new Titulo("Alem do bem e o mal"));
+        titulos.add(new Titulo("Revolucao dos bixos"));
+
+        Collections.sort(titulos);
+        System.out.println(titulos);
     }
 }
--- alura/desafioListasAula04/src/Titulo.java
+++ alura/desafioListasAula04/src/Titulo.java
@@ -1,8 +1,21 @@
 public class Titulo implements Comparable<Titulo>{
     private String nome;
 
+    public Titulo(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
     @Override
     public int compareTo(Titulo o) {
         return this.nome.compareTo(o.nome);
+    }
+
+    @Override
+    public String toString(){
+        return nome;
     }
 }
