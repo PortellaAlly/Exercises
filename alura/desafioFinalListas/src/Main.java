0902f3e55b8251b42ebbfcdcbc09e7915bffe8e8
Portella <Allysonpalmeirim12@gmail.com>
2026-07-09 18:00:35 -0300

    Fix(desafioFinalListas) | made the initializing faster (1.8 seconds now)

--- alura/desafioFinalListas/src/Main.java
+++ alura/desafioFinalListas/src/Main.java
@@ -7,13 +7,15 @@ public class Main {
         Conta conta = new Conta();
 
         System.out.print("Initializing");
-        TimeUnit.SECONDS.sleep(1);
+        TimeUnit.MILLISECONDS.sleep(300);
         System.out.print(".");
-        TimeUnit.SECONDS.sleep(1);
+        TimeUnit.MILLISECONDS.sleep(500);
         System.out.print(".");
-        TimeUnit.MILLISECONDS.sleep(1);
+        TimeUnit.MILLISECONDS.sleep(500);
         System.out.println(".");
-        TimeUnit.MILLISECONDS.sleep(1);
+        TimeUnit.MILLISECONDS.sleep(500);
+
+
 
         while(conta.getLimite() <= 0) {
             System.out.println("Digite seu limite");
