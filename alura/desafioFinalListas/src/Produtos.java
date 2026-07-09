3da60ee15110c9845a2217becdd4f055c29d8b26
Portella <Allysonpalmeirim12@gmail.com>
2026-07-09 17:46:28 -0300

    Feat(desafioFinalListas) | created class produtos

---
+++ alura/desafioFinalListas/src/Produtos.java
@@ -0,0 +1,20 @@
+public class Produtos {
+    private int preco;
+    private int nome;
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+
+    public int getNome() {
+        return nome;
+    }
+
+    public void setNome(int nome) {
+        this.nome = nome;
+    }
+}
