b1e423c19d0b68b4fce25e7d3a389fddc6c2faea
Portella <Allysonpalmeirim12@gmail.com>
2026-07-12 18:25:39 -0300

    feat(desafioApiBasicoAula02) | now you are able to convert a json even without all of the scopes

--- alura/ApiBasico/desafioApiBasicoAula02/src/Main.java
+++ alura/ApiBasico/desafioApiBasicoAula02/src/Main.java
@@ -1,8 +1,9 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setLenient().create();
 
         String jsonPessoa = "{\"nome\":\"Rodrigo\",\"idade\":20,\"cidade\":\"Brasília\"}";
         Pessoa pessoa = gson.fromJson(jsonPessoa, Pessoa.class);
