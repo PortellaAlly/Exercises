bc398ac878c6c818eb200e0af83f5daa0d741cec
Portella <Allysonpalmeirim12@gmail.com>
2026-07-12 18:30:55 -0300

    feat(desafioApiBasicoAula02) | added question 3 answer

--- alura/ApiBasico/desafioApiBasicoAula02/src/Main.java
+++ alura/ApiBasico/desafioApiBasicoAula02/src/Main.java
@@ -3,11 +3,20 @@ import com.google.gson.GsonBuilder;
 
 public class Main {
     public static void main(String[] args) {
+        // Questao 1
         Gson gson = new GsonBuilder().setLenient().create();
 
         String jsonPessoa = "{\"nome\":\"Rodrigo\",\"idade\":20,\"cidade\":\"Brasília\"}";
         Pessoa pessoa = gson.fromJson(jsonPessoa, Pessoa.class);
 
         System.out.println(pessoa);
+
+        // Questao 2
+        String jsonLivro = "{\"titulo\":\"Aventuras do Java\",\"autor\":\"Akemi\",\"editora\":{\"nome\":\"TechBooks\",\"cidade\":\"São Paulo\"}}";
+
+        Gson gson1 = new Gson();
+        Livro livro = gson1.fromJson(jsonLivro, Livro.class);
+
+        System.out.println(livro);
     }
 }
