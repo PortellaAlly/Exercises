385d5615356ed753a85592827915c2e942f4172f
Portella <Allysonpalmeirim12@gmail.com>
2026-09-04 17:18:36 -0300

    Refact | changed 'conexaoDB' file name to 'ConnectionFactory'

--- alura/JavaeJDBC/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ alura/JavaeJDBC/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class conexaoDB {
+public class ConnectionFactory {
 
     public static void main(String... x) {
 
