6d27df834de85db42e6a6b05e07d64f0dfeb7ff6
Portella <Allysonpalmeirim12@gmail.com>
2026-09-04 17:09:50 -0300

    Feat(conexaoDB) | adding a connection to my DB

---
+++ alura/JavaeJDBC/src/main/java/br/com/alura/bytebank/conexaoDB.java
@@ -0,0 +1,21 @@
+package br.com.alura.bytebank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexaoDB {
+
+    public static void main(String... x) {
+
+        try {
+            Connection connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=Allyson&password=VeryWeakPass1");
+
+            System.out.println("Conexao estabelecida");
+            connection.close();
+        } catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+}
