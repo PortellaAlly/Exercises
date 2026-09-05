25535558d063b83c8bef9599b9fe8594f7cec828
Portella <Allysonpalmeirim12@gmail.com>
2026-09-05 19:16:07 -0300

    Feat(ContaService) | now the account is going to be saved on our DB

--- alura/JavaeJDBC/src/main/java/br/com/alura/bytebank/domain/conta/DadosAberturaConta.java
+++ alura/JavaeJDBC/src/main/java/br/com/alura/bytebank/domain/conta/DadosAberturaConta.java
@@ -3,4 +3,7 @@ package br.com.alura.bytebank.domain.conta;
 import br.com.alura.bytebank.domain.cliente.DadosCadastroCliente;
 
 public record DadosAberturaConta(Integer numero, DadosCadastroCliente dadosCliente) {
+    public String dadd() {
+        return null;
+    }
 }
