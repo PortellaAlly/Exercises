c16243cb63b72cb6703bda11e4942f13136c7255
Portella <Allysonpalmeirim12@gmail.com>
2026-09-07 17:43:19 -0300

    feat(Conta) | made the realizarDeposito method work

--- alura/JavaeJDBC/src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ alura/JavaeJDBC/src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -145,5 +145,19 @@ public class ContaDAO {
     public void alterar (Integer numero, BigDecimal valor) {
         PreparedStatement preparedStatement;
         String sql = "UPDATE conta SET saldo = ? WHERE numero = ?";
+
+        try {
+            preparedStatement = conn.prepareStatement(sql);
+
+            preparedStatement.setBigDecimal(1, valor);
+            preparedStatement.setInt(2, numero);
+
+            preparedStatement.execute();
+
+            preparedStatement.close();
+            conn.close();
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
