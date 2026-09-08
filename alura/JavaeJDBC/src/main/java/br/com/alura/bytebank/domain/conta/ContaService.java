c03ce471671e65df14425f148f02bd4f0cb8d3b7
Portella <Allysonpalmeirim12@gmail.com>
2026-09-08 15:50:54 -0300

    Fix (ContaService) | realizarDeposito method no longer overlaps the old saldo value

--- alura/JavaeJDBC/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ alura/JavaeJDBC/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -56,8 +56,8 @@ public class ContaService {
         if (valor.compareTo(BigDecimal.ZERO) <= 0) {
             throw new RegraDeNegocioException("Valor do deposito deve ser superior a zero!");
         }
-        Connection conn = connection.recuperarConexao();
-        java.math.BigDecimal valorAdicionado = new ContaDAO(conn).consultarSaldo(numeroDaConta).add(valor);
+        
+        BigDecimal valorAdicionado = conta.getSaldo().add(valor);
         alterar(conta, valorAdicionado);
     }
 
