448079cb8e01a873580f2f2d89379c3189d90cdc
Portella <Allysonpalmeirim12@gmail.com>
2026-09-23 16:10:59 -0300

    Feat (Categoria) | mapping relations on JPA

---
+++ alura/JPAhibernate/src/main/java/loja/dao/CategoriaDAO.java
@@ -0,0 +1,19 @@
+package loja.dao;
+
+import loja.modelo.Categoria;
+import loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+
+public class CategoriaDAO {
+
+    private EntityManager em;
+
+    public CategoriaDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void cadastrar(Categoria categoria){
+        this.em.persist(categoria);
+    }
+}
--- alura/JPAhibernate/src/main/java/loja/testes/CadastroDeProduto.java
+++ alura/JPAhibernate/src/main/java/loja/testes/CadastroDeProduto.java
@@ -1,25 +1,31 @@
 package loja.testes;
 
+import loja.dao.CategoriaDAO;
 import loja.dao.ProdutoDAO;
 import loja.modelo.Categoria;
 import loja.modelo.Produto;
 import loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
-        Produto celular = new Produto("Xiaomi Redmi", "Bem massa visse", new BigDecimal("800"), Categoria.CELULARES);
-
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDAO dao = new ProdutoDAO(em);
+
+        Categoria celulares = new Categoria("CELULARES");
+
+        Produto celular = new Produto("Xiaomi Redmi", "Bem massa visse", new BigDecimal("800"), celulares);
+
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
 
         em.getTransaction().begin();
-        dao.cadastrar(celular);
+
+        categoriaDAO.cadastrar(celulares);
+        produtoDAO.cadastrar(celular);
+        
         em.getTransaction().commit();
         em.close();
     }
